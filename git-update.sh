039efbead3c9c267f696c6ae44b5c40de2b45450
lf8r <subhajit.dasgupta@hpe.com>
2022-01-07 11:37:32 -0600

    Added git shortcuts to bin

---
+++ git-update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git pull
+#gut
+#git submodule --quiet foreach --recursive gut
+#git submodule --quiet update --init --recursive
+#git submodule update
+#git submodule foreach git checkout master
+#git submodule foreach git pull origin master
