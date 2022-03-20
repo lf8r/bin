9bb1928d9b0cad2bca6c0f7ea72ee3a2d5ff124d
lf8r <subhajit.dasgupta@hpe.com>
2022-03-20 09:57:16 -0500

    Added script to clone all repositories.

---
+++ clone-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone git@github.com:lf8r/ab.git
+git clone git@github.com:lf8r/dbgen.git
+git clone git@github.com:lf8r/dbgen-common.git
+git clone git@github.com:lf8r/dbgen-db.git
+git clone git@github.com:lf8r/example-data.git
+git clone git@github.com:lf8r/gendb.git
+git clone git@github.com:lf8r/protogen.git
+
