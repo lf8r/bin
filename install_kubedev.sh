3acb87d97399c4fdf9de80e025ad2157f775bb69
Subhajit DasGupta <sdasgupta@M1Max.local>
2023-08-22 14:34:13 -0500

    Updated install_kubedev.sh

--- install_kubedev.sh
+++ install_kubedev.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 set -e
 
-export URL=""
-ARCH=`uname -m`
-
-if [[ "$ARCH" == "aarch64" ]]
-then
-export URL="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-fi
-
-if [[ "$ARCH" == "x86_64" ]] 
-then
-export URL="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-fi
-
-echo ${URL}
-
-if [[ "$ARCH" == "" ]]
-then
-    echo "Unsupported platform, only AMD64 and ARM64 are supported."
-    exit 1
-fi
-
-# Download kubectl
-cd /tmp
-curl -LO ${URL}
-
-# Install it
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-# Setup bash completion.
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
-
 # Install k3s
-curl -sfL https://get.k3s.io | sh -
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && chown ${USER} ~/.kube/config && chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
+mkdir -p /tmp/k3s
+cd /tmp/k3s
+curl -sfL https://get.k3s.io | sudo bash -
+mkdir -p ~/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && sudo chown ${USER}:${USER} ~/.kube/config && chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
 
 
 # Install helm
 cd /tmp && curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod +x get_helm.sh && ./get_helm.sh
 
+# Setup bash completion.
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
