ab453d0e4c33d9a54d0790b2324f058b6d88f4a5
Subhajit DasGupta <sdasgupta@M1Max.local>
2023-08-22 13:45:53 -0500

    Renamed kubectl installation script, added helm and helm (bash completion).

--- install_kubedev.sh
+++ install_kubedev.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Install helm
+cd /tmp && curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod +x get_helm.sh && ./get_helm.sh
+
 export URL=""
 ARCH=`uname -m`
 
