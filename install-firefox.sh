e7ab9266faf29a0a452e20cc0c84d0ca09ab2274
lf8r <subhajit.dasgupta@hpe.com>
2024-05-14 15:09:58 -0500

    Corrected install-firefox.sh

--- install-firefox.sh
+++ install-firefox.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-set -e
-sudo snap remove -y firefox
+sudo snap remove firefox
 sudo apt remove -y firefox
 sudo add-apt-repository -y ppa:mozillateam/ppa
 
 FILE="/etc/apt/preferences.d/mozillateamppa"
-
-sudo /bin/cat <<EOM >$FILE
+set -e
+/bin/cat <<EOM >/tmp/mozillateamppa
 Package: firefox*
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 EOM
 
+sudo mv /tmp/mozillateamppa $FILE
+
 sudo apt update
-sudo apt -y install firefox
+sudo apt install -y firefox
 
 
