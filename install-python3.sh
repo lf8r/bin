2e3523d995e66dc999d43260b3133e0d82e25d2c
lf8r <subhajit.dasgupta@hpe.com>
2024-06-13 08:46:29 -0500

    Update install-python3.sh to add a line to install pip3.11

--- install-python3.sh
+++ install-python3.sh
@@ -4,4 +4,5 @@ sudo apt -y install python3-venv
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.11
 sudo apt install python3.11-venv
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 
