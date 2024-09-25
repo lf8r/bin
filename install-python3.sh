8cd8405d81bbc03462114c35b02f11b5e351f005
lf8r <subhajit.dasgupta@hpe.com>
2024-09-24 20:05:46 -0500

    Install the python-3.11-tk package to enable the use of the tkinter module in Python 3.11.

--- install-python3.sh
+++ install-python3.sh
@@ -5,6 +5,6 @@ sudo apt -y install python3-venv
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.11
 sudo apt install python3.11-venv
-sudo apt-get install python3-tk
+sudo apt-get install python3.11-tk
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 
