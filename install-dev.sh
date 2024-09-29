b34c5f48a5dad1c54800f88728a18c60e6b20e38
lf8r <subhajit.dasgupta@hpe.com>
2024-09-29 05:58:01 -0500

    Install virt-manager by default in install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt install -y curl ssh net-tools build-essential libgeos-dev
+sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop
 install-postgres-client16.sh
 install-python3.sh
 install-firefox.sh
