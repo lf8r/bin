928ea0119d460a6b33eeb3bd366396e9ad33c787
lf8r <subhajit.dasgupta@hpe.com>
2024-10-09 10:58:21 -0500

    Added install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+sudo rm -rf /user/local/go
+ARCH=$(dpkg --print-architecture)
+wget https://go.dev/dl/go1.21.7.linux-${ARCH}.tar.gz
+sudo tar -C /usr/local -zxvf go1.21.7.linux-${ARCH}.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+mkdir -p ~/go/bin
+#go install golang.org/dl/go1.22.7@latest
