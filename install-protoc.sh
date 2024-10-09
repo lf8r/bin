02a09a8a104338f8e55bfa6be441f83d82e2f595
lf8r <subhajit.dasgupta@hpe.com>
2024-10-09 18:08:07 -0500

    New file

---
+++ install-protoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+sudo apt install -y protobuf-compiler
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
