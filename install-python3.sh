aaa286bc43791e5dfda7dc3ebb1beac2af363284
lf8r <subhajit.dasgupta@hpe.com>
2025-01-14 07:16:50 -0600

    Updated install-python3.sh with install of libavif16

--- install-python3.sh
+++ install-python3.sh
@@ -6,5 +6,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.11
 sudo apt install python3.11-venv
 sudo apt-get install python3.11-tk
+# Required for web browser automation with playwright (see https://playwright.dev/python/docs/intro).
+sudo apt-get install libavif16
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 
