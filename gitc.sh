07164a1f5c19b695a14f137b3aacafd01a4527c7
lf8r <subhajit.dasgupta@hpe.com>
2025-02-06 12:46:05 -0600

    Updated files to support git completion in bash.

--- gitc.sh
+++ gitc.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-
-echo 'if [ -f ~/.git-completion.bash ]; then
-    . ~/.git-completion.bash
-fi' >> ~/.bash_profile
-
-$(BASH) exec
+cp git-completion.sh ~/.git-completion.bash
+echo "source ~/.bash_profile"
