38cff8d84341fecfe13de0c90d9886acfca687e5
lf8r <subhajit.dasgupta@hpe.com>
2025-02-08 06:29:09 -0600

    Added install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.11-jbr
+sdk use java 17.0.11-jbr
+exec "$BASH"
+which java
