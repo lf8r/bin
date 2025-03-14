7cc55245643944ca92440ccbfd623483e1f4e119
lf8r <subhajit.dasgupta@hpe.com>
2025-03-14 17:43:49 +0000

    Added a command to mount TNAS from Linux.

--- install-go.sh
+++ install-go.sh
@@ -2,8 +2,8 @@
 set -e
 sudo rm -rf /user/local/go
 ARCH=$(dpkg --print-architecture)
-wget https://go.dev/dl/go1.21.7.linux-${ARCH}.tar.gz
-sudo tar -C /usr/local -zxvf go1.21.7.linux-${ARCH}.tar.gz
+wget https://go.dev/dl/go1.24.0.linux-${ARCH}.tar.gz
+sudo tar -C /usr/local -zxvf go1.24.0.linux-${ARCH}.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 mkdir -p ~/go/bin
 #go install golang.org/dl/go1.22.7@latest
