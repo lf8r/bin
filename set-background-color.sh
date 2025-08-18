e8a1295b8d889c334548863ba72bb05063b61f1d
subhajit-dasgupta_hpeprod <subhajit.dasgupta@hpe.com>
2025-08-18 11:19:35 -0500

    Update set-background-color

--- set-background-color.sh
+++ set-background-color.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.background primary-color '#AAAAAA'
+gsettings set org.gnome.desktop.background picture-uri none
+gsettings set org.gnome.desktop.background picture-uri-dark none
+gsettings set org.gnome.desktop.background primary-color '#5C4033'
