320a30da6be736fdf164d48311ea2be11ce0f491
lf8r <subhajit.dasgupta@hpe.com>
2025-11-11 19:45:54 -0600

    New files.
    Signed-off-by: lf8r <subhajit.dasgupta@hpe.com>

---
+++ start_ollama_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+export OLLAMA_HOST="0.0.0.0"
+export OLLAMA_MAX_LOADED_MODELS=6
+export OLLAMA_NUM_PARALLEL=4
+export OLLAMA_MAX_QUEUE=512
+export OLLAMA_CONTEXT_LENGTH=16384
+pkill ollama || true
+ollama serve
+
