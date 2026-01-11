276ecadb5b925661eebed645e597e1d91d4271d6
lf8r <subhajit_dasgupta@yahoo.com>
2026-01-11 05:26:13 -0600

    Updated install-dev.sh with instructions to enable focus stealing.

--- change-speed.sh
+++ change-speed.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
+sudo apt install -y dbus-x11
 sudo nm-connection-editor
--- install-dev.sh
+++ install-dev.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11
+sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11 sudo apt-get install compizconfig-settings-manager
 git lfs install
 install-postgres-client16.sh
 install-python3.sh
 install-firefox.sh
 install-docker-ce.sh
+
+# Notes
+# Run ccsm and set General options -> Focus and Raise Behavior -> Focus Prevention Level to "Off"
