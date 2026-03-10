70e620f1793174d9b1c2c56f3af4670062ab5f5d
lf8r <subhajit_dasgupta@yahoo.com>
2026-03-10 06:05:14 -0500

    Added libsecret-tools to install-dev.sh.

--- install-dev.sh
+++ install-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11 compizconfig-settings-manager
+sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11 compizconfig-settings-manager libsecret-tools
 sudo apt install -y gnome-keyring libsecret-1-0 libsecret-tools
 git lfs install
 install-postgres-client16.sh
