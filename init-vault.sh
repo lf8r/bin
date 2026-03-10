3fb0b3dc8533e1e8389601e9288fbf5950a5fb14
lf8r <subhajit_dasgupta@yahoo.com>
2026-03-10 06:03:47 -0500

    Added install-vault.sh

--- init-vault.sh
+++ init-vault.sh
@@ -3,3 +3,10 @@ set -e
 export VAULT_ADDR=https://127.0.0.1:8200
 export VAULT_SKIP_VERIFY=true
 vault operator init -key-shares=1 -key-threshold=1
+
+echo "Vault initialized. Please save the unseal key and root token displayed above. You can use the unseal key to unseal the Vault server and the root token to log in and manage Vault."
+echo "Save these values in your setenv file for future use:"
+echo "export VAULT_UNSEAL_KEY=\"<your-unseal-key>\""
+echo "export VAULT_ROOT_TOKEN=\"<your-root-token>\""
+echo "\n\n"
+echo "Run vault operator unseal <your-unseal-key> to unseal the Vault server, and vault login <your-root-token> to log in with the root token."
--- install-dev.sh
+++ install-dev.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11 sudo apt-get install compizconfig-settings-manager
+sudo apt install -y curl ssh net-tools build-essential libgeos-dev virt-manager ack htop time git-lfs dbus-x11 compizconfig-settings-manager
+sudo apt install -y gnome-keyring libsecret-1-0 libsecret-tools
 git lfs install
 install-postgres-client16.sh
 install-python3.sh
 install-firefox.sh
 install-docker-ce.sh
+install-vault.sh
 
 # Notes
 # Run ccsm and set General options -> Focus and Raise Behavior -> Focus Prevention Level to "Off"
