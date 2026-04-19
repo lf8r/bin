6b88fa5b710bdd3122b4b41a415ba58604f2599a
lf8r <subhajit_dasgupta@yahoo.com>
2026-04-18 22:19:09 -0500

    Cleanup wallpaper

--- wallpaper/run.sh
+++ wallpaper/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . ./activate.sh
+pip install pillow
 HOSTIP=$(./get_ip.sh) python make_hostname_wallpaper.py
 
 echo "Go to Settings|Appearance|Background, add the picture located at $HOME/Pictures/hostname_wallpaper.png and set it as the background."
