f5457da38041ae93381c45c5db26525b4e983731
lf8r <subhajit.dasgupta@hpe.com>
2026-05-03 13:34:03 -0500

    Renamed and modify ollama management files for MacOS.

---
+++ ollama_start_mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+OLLAMA_MLX=1 OLLAMA_HOST="0.0.0.0" OLLAMA_KEEP_ALIVE=5m OLLAMA_NUM_PARALLEL=4 OLLAMA_DEBUG=BUG=1 OLLAMA_FLASH_ATTENTION="1"  ollama serve 2>&1 | tee -a ~/.ollama/logs/server.log &
+echo $! > ~/.ollama/pid
---
+++ ollama_stop_mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+killall ollama
+rm ~/.ollama/pid
