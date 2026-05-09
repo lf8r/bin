d0e20ac77b7f11c50e8795d7e0233ec109e7646e
lf8r <subhajit.dasgupta@hpe.com>
2026-05-09 18:23:25 -0500

    Updated ollama_start_mac.sh to use the full path to the ollama binary and removed the redirection to the log file. This allows for better debugging and ensures that the correct version of ollama is used.

--- ollama_start_mac.sh
+++ ollama_start_mac.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
-OLLAMA_MLX=1 OLLAMA_HOST="0.0.0.0" OLLAMA_KEEP_ALIVE=5m OLLAMA_NUM_PARALLEL=4 OLLAMA_DEBUG=BUG=1 OLLAMA_FLASH_ATTENTION="1"  ollama serve 2>&1 | tee -a ~/.ollama/logs/server.log &
+
+# This command uses a 256K context length.
+# OLLAMA_MLX=1 OLLAMA_HOST="0.0.0.0" OLLAMA_KEEP_ALIVE=5m OLLAMA_NUM_PARALLEL=4 OLLAMA_DEBUG=1 OLLAMA_FLASH_ATTENTION="1" OLLAMA_CONTEXT_LENGTH=262144 ollama serve 2>&1 | tee -a ~/.ollama/logs/server.log &
+
+# This command uses a 128K context length.
+# OLLAMA_MLX=1 OLLAMA_HOST="0.0.0.0" OLLAMA_KEEP_ALIVE=5m OLLAMA_NUM_PARALLEL=4 OLLAMA_DEBUG=1 OLLAMA_FLASH_ATTENTION="1" OLLAMA_CONTEXT_LENGTH=131072  /opt/homebrew/opt/ollama/bin/ollama serve 2>&1 | tee -a ~/.ollama/logs/server.log &
+OLLAMA_MLX=1 OLLAMA_HOST="0.0.0.0" OLLAMA_KEEP_ALIVE=5m OLLAMA_NUM_PARALLEL=4 OLLAMA_DEBUG=1 OLLAMA_FLASH_ATTENTION="1" OLLAMA_CONTEXT_LENGTH=131072  /opt/homebrew/opt/ollama/bin/ollama serve
+
 echo $! > ~/.ollama/pid
