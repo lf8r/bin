309c1cdc8346a8760be91c5dc338bcdd69f229f5
lf8r <subhajit_dasgupta@yahoo.com>
2026-05-18 10:21:08 -0500

    Combined vscode and vscode-insiders installers into a single script.

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set +e
-sudo apt purge code
+sudo apt -y purge code code-insiders
+sudo apt -y autoremove
 set -e
 # rm -rf ~/.vscode/extensions
 sudo apt update
 sudo apt install wget gpg software-properties-common apt-transport-https -y
-
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/microsoft.gpg > /dev/null
-
 echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-
+#echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
-
-sudo apt install code
+sudo apt install -y code code-insiders
 
