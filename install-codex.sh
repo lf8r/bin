5a4109ac0ae9730b6d422815b61b59a8837cf392
lf8r <subhajit_dasgupta@yahoo.com>
2026-06-05 09:58:34 -0500

    Update install-codex.sh

--- install-codex.sh
+++ install-codex.sh
@@ -9,7 +9,6 @@ touch ~/.codex/config.toml
 cp ~/bin/config.toml ~/.codex/config.toml
 cd ~
 npx skills@latest add mattpocock/skills
-rsync -Partv ~/bin/codex/commit-message ~/.codex/skills
 # git clone https://github.com/hyhmrright/brooks-lint.git /tmp/brooks-lint
 # mkdir -p ~/.codex/skills/brooks-lint
 # cp -r /tmp/brooks-lint/skills/* ~/.codex/skills/brooks-lint/
